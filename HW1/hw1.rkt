9d28a1310a14773614a11db03763dd1827e0a0f1
user <user@Andrews-MacBook-Pro.local>
2019-01-14 20:31:02 -0800

    adding problem 4, 5e

--- HW1/hw1.rkt
+++ HW1/hw1.rkt
@@ -32,10 +32,13 @@
 (define (unzip l)
     (list (everyotherIndex l) (everyotherIndex (cdr l))))
 
-
-
-
-
+;; Problem 4
+(define (expand lst)
+   (cond ((null? lst) '())
+         ((not (pair? (car lst))) (cons (car lst) (expand (cdr lst))))
+         ((< (caar lst) 1) (expand (cdr lst)))
+         (else (cons (cadar lst) (expand (cons (cons (- (caar lst) 1) (cdar lst))
+                                                (cdr lst)))))))
 
 ;; Problem 5a
 (define (value l)
@@ -58,3 +61,27 @@
     (cond ((null? tree) #f)
           ((equal? item (value tree)) #t)
           (else (or (contains item (left tree)) (contains item (right tree))))))
+
+;; Problem 5d
+
+
+
+;; Problem 5e
+(define (inorder tree)
+    (if (null? tree)
+        '()
+        (append (inorder (left tree))
+                (list (value tree))
+                (inorder (right tree)))))
+
+(define (sorted lst)
+    (cond ((null? lst) #t)
+          ((eq? (length lst) 1) #t)
+          ((> (cadr lst) (car lst)) (sorted (cdr lst)))
+          (else #f)))
+
+(define (isBST tree)
+    (let ((order (inorder tree)))
+      (if (eq? #t order)
+          #t
+          #f)))
