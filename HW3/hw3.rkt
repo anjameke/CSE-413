ada3e4123d720b4caa3b8271eff69cdd314554a4
user <user@Andrews-MacBook-Pro.local>
2019-01-26 21:35:26 -0800

    added helper/abstraction function to hid implemenation details in main diff func

--- HW3/hw3.rkt
+++ HW3/hw3.rkt
@@ -1,7 +1,15 @@
 (define (get-op E) (car E))
 
+(define (diff-constant x E)
+    0)
+(define (same-wrt-var v1 v2)
+    (if (eq? v1 v2) 1 0))
+
 (define (diff-sum x E)
-    (list '+ (diff x (cadr E)) (diff x (caddr E))))
+    (make-lst (diff x (cadr E)) (diff x (caddr E))))
+
+(define (make-lst lst1 lst2)
+    (list '+ lst1 lst2))
 
 (define diff-dispatch
    (list (list '+ diff-sum)))
@@ -10,7 +18,6 @@
   (cadr (assoc op table)))
 
 (define (diff v E)
-  (cond ((number? E) 0)
-        ((symbol? E)
-         (if (eq? v E) 1 0))
+  (cond ((number? E) (diff-constant v E))
+        ((symbol? E) (same-wrt-var v E))
         (else ((get-op-lst-from-table (get-op E) diff-dispatch) v E))))
